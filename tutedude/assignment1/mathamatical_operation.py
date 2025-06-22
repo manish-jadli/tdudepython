e9a95a1f45d9dfb7d45001b0c6662cd7bd9f6fb8
Manish Jadli <manish.jadli@irasys.biz>
2025-06-22 18:47:14 +0530

    assigment 1 complete

---
+++ tutedude/assignment1/mathamatical_operation.py
@@ -0,0 +1,19 @@
+#Takes two numbers as input from the user
+firstInput=int(input("Enter the first number: "))
+secondInput=int(input("Enter the second number: "))
+
+print(firstInput)
+print(secondInput)
+
+# Performs the basic mathematical operations on these two numbers
+
+additionalCalc= firstInput + secondInput
+subtractionCalc= firstInput - secondInput
+multiplicatonCalc=firstInput * secondInput
+divisionCalc=firstInput / secondInput
+
+print("Additon: ", additionalCalc)
+print("Substraction: ",subtractionCalc)
+print("Multiplication: ", multiplicatonCalc)
+print(("Division: "), divisionCalc)
+
---
+++ tutedude/assignment1/personalized_greeting.py
@@ -0,0 +1,13 @@
+#Takes a user's first name and last name as input.
+firstName=input("Enter your first name: ")
+lastName=input("Enter your last name: ")
+
+print(firstName)
+print(lastName)
+
+#Concatenates the first name and last name into a full name.
+#Prints a personalized greeting message using the full name.
+
+print("Hello,",firstName + " " + lastName + "! " + "welcome to the python program.")
+
+
